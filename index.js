9adc4ac9e0cf3cf889adc0bf38aa211227944c9c
Marc Obel <marcentusch@hotmail.com>
2024-06-06 09:45:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const path = require("path");
 
 const inputDir = "./admatic-input-images";
 const outputDir = "./admatic-output-images";
-const width = 800;
-const height = 800;
+
+const args = process.argv.slice(2);
+
+if (args.length !== 2 || isNaN(args[0]) || isNaN(args[1])) {
+  console.error("Usage: node index.js <width> <height>");
+  process.exit(1);
+}
+
+const width = Number(args[0]);
+const height = Number(args[1]);
 
 fs.ensureDirSync(outputDir);
 
 const resizeImages = async () => {
   try {
     const files = await fs.readdir(inputDir);
+
+    console.log(`Found ${files.length} images, starting resizing...`);
 
     const imageFiles = files.filter((file) =>
       /\.(jpg|jpeg|webp|png)$/i.test(file)
